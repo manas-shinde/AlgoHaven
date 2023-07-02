940ae1485dc28d5d4f6b16f91e287efd1be1597a
manas-shinde <manas.a.shinde11@gmail.com>
2023-07-02 12:11:41 +0530

    check leap year or not.

---
+++ src/IntroTopics/LeapYearOrNot.java
@@ -0,0 +1,28 @@
+package IntroTopics;
+
+import java.util.Scanner;
+
+public class LeapYearOrNot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Year to check its leap year or not :");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("Leap Year!!");
+        } else {
+            System.out.println("Not Leap Year!!");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        /**
+         * Checks if a given year is a leap year.
+         *
+         * @param year The year to be checked.
+         * @return True if the year is a leap year, false otherwise.
+         */
+        return ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+    }
+}

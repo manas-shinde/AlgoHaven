7543bab1fc1134955ed7bba46befede9d98fc9d4
manas-shinde <manas.a.shinde11@gmail.com>
2023-07-02 12:52:18 +0530

    leetcode question 1281.

---
+++ src/IntroTopics/subtractProductAndSum.java
@@ -0,0 +1,28 @@
+package IntroTopics;
+
+import java.util.Scanner;
+
+/**
+ * LeetCode question link -> https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/description/
+ */
+public class subtractProductAndSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number : ");
+        int number = scanner.nextInt();
+        int productOfDigits = 1, sumOfDigit = 0, digit;
+
+        while (number != 0) {
+            digit = number % 10;
+            productOfDigits *= digit;
+            sumOfDigit += digit;
+            number /= 10;
+        }
+
+        System.out.println("Product of Digits :" + productOfDigits);
+        System.out.println("Sum of Digits :" + sumOfDigit);
+        System.out.println("Result :" + (productOfDigits - sumOfDigit));
+
+
+    }
+}

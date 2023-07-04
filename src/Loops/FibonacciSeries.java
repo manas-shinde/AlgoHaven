53144185363c7c578a3a52e148cae11468ca2ec2
manas-shinde <manas.a.shinde11@gmail.com>
2023-07-04 10:16:06 +0530

    Basics program to revise the concepts.

---
+++ src/Loops/FibonacciSeries.java
@@ -0,0 +1,25 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Number :");
+        int n = scanner.nextInt();
+
+        int a = 0, b = 1, c;
+
+        System.out.print(a + " " + b + ' ');
+
+        while (n != 0) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+
+
+            n--;
+        }
+    }
+}

0c650b4791c1d720d916078d124c89581264cfe4
manas-shinde <manas.a.shinde11@gmail.com>
2023-07-08 11:31:46 +0530

    Basic Problem.

---
+++ src/array/MinAndMax.java
@@ -0,0 +1,42 @@
+package array;
+
+public class MinAndMax {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        ReverseArray.fillArray(arr);
+
+        ReverseArray.display(arr);
+
+        System.out.println("Minimum in array :" + findMinimum(arr));
+        System.out.println("Maximum in array : " + findMaximum(arr));
+    }
+
+    /**
+     * Finds the minimum value in the given integer array.
+     *
+     * @param arr The integer array to search for the minimum value.
+     * @return The minimum value found in the array.
+     */
+    public static int findMinimum(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int val : arr) {
+            if (val < min) min = val;
+        }
+        return min;
+    }
+
+    /**
+     * Finds the maximum value in the given integer array.
+     *
+     * @param arr The integer array to search for the maximum value.
+     * @return The maximum value found in the array.
+     */
+    public static int findMaximum(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int val : arr) {
+            if (val > max) max = val;
+        }
+        return max;
+    }
+}

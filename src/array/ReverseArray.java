5529bb936c78a8e2c2b0eda80a3899f4c0cffe80
manas-shinde <manas.a.shinde11@gmail.com>
2023-07-09 12:31:21 +0530

    extracted some common methods to separate class.

--- src/array/ReverseArray.java
+++ src/array/ReverseArray.java
@@ -1,22 +1,20 @@
 package array;
 
-import java.util.Scanner;
-
 public class ReverseArray {
     public static void main(String[] args) {
 
         int[] arr = new int[5];
 
-        fillArray(arr);
+        ArrayMethods.fillArray(arr);
 
         System.out.println("Original Array: ");
-        display(arr);
+        ArrayMethods.display(arr);
 
 //        int[] revArr = reverseArray1(arr);
 
         reverse2(arr);
         System.out.println("Reverse Array : ");
-        display(arr);
+        ArrayMethods.display(arr);
 
     }
 
@@ -52,34 +50,5 @@ public class ReverseArray {
             revArr[j] = arr[i];
         }
         return revArr;
-    }
-
-    /**
-     * Displays the elements of the given integer array.
-     *
-     * @param arr The integer array to be displayed.
-     */
-    public static void display(int[] arr) {
-        for (int val : arr) {
-            System.out.print(val + " ");
-        }
-        System.out.println();
-    }
-
-    /**
-     * Fills the elements with the given integer array by taking input from the user.
-     *
-     * @param arr The integer array to be filled.
-     */
-    public static void fillArray(int[] arr) {
-        Scanner scanner = new Scanner(System.in);
-
-        int arrayLength = arr.length;
-
-        System.out.println("Enter values to be fill in the array ");
-        for (int i = 0; i < arrayLength; i++) {
-            System.out.print((i + 1) + " value :");
-            arr[i] = scanner.nextInt();
-        }
     }
 }
